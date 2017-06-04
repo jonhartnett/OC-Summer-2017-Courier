2e046ced32b8e3012f9c7258c9b1a5739d9de2ad
Jonathan Troyer <jonathantroyer@outlook.com>
2017-06-04 17:25:50 -0500

    Add login method stub

--- src/main/java/edu/oc/courier/ui/LoginController.java
+++ src/main/java/edu/oc/courier/ui/LoginController.java
@@ -1,14 +1,28 @@
 package edu.oc.courier.ui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LoginController implements Initializable {
 
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField password;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+    }
+
+    public void login(ActionEvent actionEvent) {
+        //Authenticate based on username/password here
     }
 }
