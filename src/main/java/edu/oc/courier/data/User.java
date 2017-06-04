e690c5261e242c4d43f43ea52d32d036047e2a6e
DemonWav <demonwav@gmail.com>
2017-06-04 17:47:48 -0500

    Add password hashing and salt generation to user

--- src/main/java/edu/oc/courier/data/User.java
+++ src/main/java/edu/oc/courier/data/User.java
@@ -1,17 +1,60 @@
 package edu.oc.courier.data;
 
 import com.google.common.base.MoreObjects;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Throwables;
+import java.security.MessageDigest;
 import java.util.Arrays;
 import java.util.Objects;
+import java.util.Random;
 
 @SuppressWarnings("unused")
 public final class User {
+
+    private static final String alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_";
+    private static final Random rand = new Random();
+    private static final MessageDigest digest;
 
     private String name;
     private int id;
     private byte[] password;
     private String salt;
     private UserType type;
+
+    public boolean isPasswordValid(String password) {
+        Preconditions.checkNotNull(password, "password must not be null");
+
+        final byte[] bytes;
+        try {
+            bytes = digest.digest((salt + password).getBytes("UTF-8"));
+        } catch (Exception e) {
+            Throwables.throwIfUnchecked(e);
+            throw new RuntimeException(e);
+        }
+
+        return Arrays.equals(this.password, bytes);
+    }
+
+    public void setPassword(String password) {
+        Preconditions.checkNotNull(password, "password must not be null");
+        generateSalt();
+
+        try {
+            this.password = digest.digest((salt + password).getBytes("UTF-8"));
+        } catch (Exception e) {
+            Throwables.throwIfUnchecked(e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void generateSalt() {
+        if (salt != null) {
+            return;
+        }
+
+        salt = rand.ints(16, 0, alpha.length()).map(alpha::charAt)
+            .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append).toString();
+    }
 
     public String getName() {
         return name;
@@ -83,5 +126,14 @@ public final class User {
             .add("salt", salt)
             .add("type", type)
             .toString();
+    }
+
+    static {
+        try {
+            digest = MessageDigest.getInstance("SHA-512");
+        } catch (Exception e) {
+            Throwables.throwIfUnchecked(e);
+            throw new RuntimeException(e);
+        }
     }
 }
