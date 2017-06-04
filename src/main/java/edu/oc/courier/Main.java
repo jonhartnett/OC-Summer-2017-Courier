701cdfc99a92e820b6d79c01c3d4a2ebd02adace
Jonathan Troyer <jonathantroyer@outlook.com>
2017-06-04 16:16:26 -0500

    Add base container

--- src/main/java/edu/oc/courier/Main.java
+++ src/main/java/edu/oc/courier/Main.java
@@ -1,7 +1,29 @@
 package edu.oc.courier;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource("ui/container.fxml"));
+            primaryStage.setScene(new Scene(root, 500, 400));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        primaryStage.setTitle("Courier service");
+        primaryStage.show();
     }
 }
