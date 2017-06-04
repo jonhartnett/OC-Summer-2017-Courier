a9a1732fb9f5322dfdc1fbff426fcfc3849e8b31
DemonWav <demonwav@gmail.com>
2017-06-04 16:05:34 -0500

    Add basic data classes

---
+++ src/main/java/edu/oc/courier/data/Client.java
@@ -0,0 +1,75 @@
+package edu.oc.courier.data;
+
+import com.google.common.base.MoreObjects;
+import java.util.Objects;
+
+@SuppressWarnings("unused")
+public final class Client {
+
+    private int id;
+    private String name;
+    private String address;
+    private String deliveryInstructions;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDeliveryInstructions() {
+        return deliveryInstructions;
+    }
+
+    public void setDeliveryInstructions(String deliveryInstructions) {
+        this.deliveryInstructions = deliveryInstructions;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final Client client = (Client) o;
+        return id == client.id &&
+            Objects.equals(name, client.name) &&
+            Objects.equals(address, client.address) &&
+            Objects.equals(deliveryInstructions, client.deliveryInstructions);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, address, deliveryInstructions);
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+            .add("id", id)
+            .add("name", name)
+            .add("address", address)
+            .add("deliveryInstructions", deliveryInstructions)
+            .toString();
+    }
+}
---
+++ src/main/java/edu/oc/courier/data/Driver.java
@@ -0,0 +1,53 @@
+package edu.oc.courier.data;
+
+import com.google.common.base.MoreObjects;
+import java.util.Objects;
+
+@SuppressWarnings("unused")
+public final class Driver {
+
+    private String name;
+    private int id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final Driver driver = (Driver) o;
+        return id == driver.id &&
+            Objects.equals(name, driver.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, id);
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+            .add("name", name)
+            .add("id", id)
+            .toString();
+    }
+}
