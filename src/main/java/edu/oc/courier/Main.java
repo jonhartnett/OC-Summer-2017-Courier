a9bc4d5ff0f790e1b70bfacb033df1ebe8a19dbd
Jonathan Troyer <jonathantroyer@outlook.com>
2017-06-04 16:24:00 -0500

    Refactor container.fmlx

--- src/main/java/edu/oc/courier/Main.java
+++ src/main/java/edu/oc/courier/Main.java
@@ -18,7 +18,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) {
         Parent root;
         try {
-            root = FXMLLoader.load(getClass().getResource("ui/container.fxml"));
+            root = FXMLLoader.load(getClass().getResource("/container.fxml"));
             primaryStage.setScene(new Scene(root, 500, 400));
         } catch (IOException e) {
             e.printStackTrace();
