d30211a907315c0c666247502c085642519fba6d
jonhartnett <jonehartnett@yahoo.com>
2017-06-04 20:38:57 -0500

    Add example persistence code

--- src/main/java/edu/oc/courier/Main.java
+++ src/main/java/edu/oc/courier/Main.java
@@ -6,9 +6,12 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import java.io.IOException;
 
 public class Main extends Application {
+    public static final EntityManagerFactory ENTITY_MANAGER_FACTORY = Persistence.createEntityManagerFactory("courier-types");
 
     public static void main(String[] args) {
         launch(args);
@@ -20,5 +23,23 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 500, 400));
         primaryStage.setTitle("Courier service");
         primaryStage.show();
+
+//        EntityManager man = ENTITY_MANAGER_FACTORY.createEntityManager();
+//        man.getTransaction().begin();
+//
+//        Invoice inv = man.find(Invoice.class, 1);
+//        System.out.println(inv);
+//        Client client = new Client();
+//        client.setName("MegaCorp");
+//        client.setAddress("Broadway");
+//
+//        Invoice invoice = new Invoice();
+//        invoice.setClient(client);
+//
+//        man.persist(invoice);
+//        Driver driver = new Driver();
+//        driver.setName("Tim");
+//        man.persist(driver);
+//        man.getTransaction().commit();
     }
 }
