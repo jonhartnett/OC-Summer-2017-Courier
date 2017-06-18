baebf2435855c19a1c3d34a7c1d892da440d5d1d
Jonathan Troyer <jonathantroyer@outlook.com>
2017-06-18 12:21:08 -0500

    Add error handling to login

--- src/main/java/edu/oc/courier/ui/LoginController.java
+++ src/main/java/edu/oc/courier/ui/LoginController.java
@@ -5,6 +5,7 @@ import edu.oc.courier.data.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
@@ -16,9 +17,12 @@ import java.util.ResourceBundle;
 public class LoginController implements Initializable {
 
     public static User currentUser;
+    public Label error;
 
-    @FXML private TextField username;
-    @FXML private PasswordField password;
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField password;
 
     private ContainerController container;
 
@@ -35,22 +39,26 @@ public class LoginController implements Initializable {
     private void login(ActionEvent actionEvent) throws IOException {
         final String usernameText = this.username.getText();
         final Optional<User> userOpt = DB.single(DB.m().createQuery("SELECT u FROM User u WHERE u.username = :username", User.class)
-            .setParameter("username", usernameText));
+                .setParameter("username", usernameText));
 
         if (!userOpt.isPresent()) {
-            // No user
-            throw new UnsupportedOperationException("TODO");
-        }
-
-        final User user = userOpt.get();
-        if (user.isPasswordValid(password.getText())) {
-            // correct login
-            container.menu.setDisable(false);
-            currentUser = user;
-            container.loadScreen("ticket");
+            reset();
+            error.setText("User not found");
         } else {
-            // incorrect login
-            throw new UnsupportedOperationException("TODO");
+            final User user = userOpt.get();
+            if (user.isPasswordValid(password.getText())) {
+                container.menu.setDisable(false);
+                currentUser = user;
+                container.loadScreen("ticket");
+            } else {
+                reset();
+                error.setText("Invalid login. Please check the password.");
+            }
         }
+    }
+
+    private void reset() {
+        username.clear();
+        password.clear();
     }
 }
