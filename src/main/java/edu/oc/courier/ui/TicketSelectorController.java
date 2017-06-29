f7a4004a263e09fea049a1bfcc72386081fc199b
Jonathan Troyer <jonathantroyer@outlook.com>
2017-06-29 15:26:33 -0500

    Make ticket selections sortable

--- src/main/java/edu/oc/courier/ui/TicketSelectorController.java
+++ src/main/java/edu/oc/courier/ui/TicketSelectorController.java
@@ -5,7 +5,12 @@ import edu.oc.courier.DBTransaction;
 import edu.oc.courier.data.Ticket;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.FlowPane;
+import javafx.util.Callback;
 
 import java.io.IOException;
 import java.net.URL;
@@ -13,25 +18,76 @@ import java.util.ResourceBundle;
 
 public class TicketSelectorController implements Initializable {
 
+    private class Tuple<X, Y> {
+        public X x;
+        public Y y;
+
+        public Tuple(X x, Y y){
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    @FXML private ComboBox<Tuple<String, String>> sortOrder;
+    @FXML private ToggleGroup order;
     @FXML private FlowPane tickets;
 
     private ContainerController controller;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        try (DBTransaction transaction = DB.getTransation()) {
-            transaction.getAll(transaction.query("SELECT t FROM Ticket t", Ticket.class).setMaxResults(100))
-                .forEach(ticket ->
-                    this.tickets.getChildren().add(new TicketSelectionController(ticket, this))
-                );
-        }
+        sortOrder.setCellFactory(new Callback<ListView<Tuple<String, String>>, ListCell<Tuple<String, String>>>() {
+            @Override
+            public ListCell<Tuple<String, String>> call(ListView<Tuple<String, String>> param) {
+                return new ListCell<Tuple<String, String>>() {
+                    @Override
+                    public void updateItem(Tuple<String, String> tuple, boolean isEmpty) {
+                        super.updateItem(tuple, isEmpty);
+                        if(tuple != null)
+                            setText(tuple.x);
+                    }
+                };
+            }
+        });
+        sortOrder.setButtonCell(sortOrder.getCellFactory().call(null));
+        sortOrder.getItems().addAll(
+                new Tuple<>("Ticket number", "id"),
+                new Tuple<>("Order time", "orderTime"),
+                new Tuple<>("Order taker", "orderTaker"),
+                new Tuple<>("Pickup client", "pickupClient"),
+                new Tuple<>("Requested pickup time", "pickupTime"),
+                new Tuple<>("Delivery client", "deliveryClient"),
+                new Tuple<>("Estimated delivery time", "estDeliveryTime"),
+                new Tuple<>("Estimated distance", "estDistance"),
+                new Tuple<>("Courier", "courier"),
+                new Tuple<>("Assigned leave time", "leaveTime"),
+                new Tuple<>("Actual pickup time", "actualPickupTime"),
+                new Tuple<>("Actual delivery time", "actualDeliveryTime")
+        );
+        updateTickets();
     }
 
-    public void setController(ContainerController controler) {
-        this.controller = controler;
+    public void setController(ContainerController controller) {
+        this.controller = controller;
     }
 
     public void select(Ticket ticket) throws IOException {
         controller.setCenter(new TicketController(ticket));
+    }
+
+    @FXML
+    private void updateTickets() {
+        String query = "SELECT t FROM Ticket t";
+        Tuple<String, String> field = sortOrder.getSelectionModel().getSelectedItem();
+        if (field != null && !field.y.equals("")) {
+            query += " ORDER BY t." + field.y + " " + order.getSelectedToggle().getUserData();
+        }
+        this.tickets.getChildren().clear();
+        try (DBTransaction transaction = DB.getTransation()) {
+            transaction.getAll(transaction.query(query, Ticket.class).setMaxResults(100))
+                .forEach(ticket ->
+                    this.tickets.getChildren().add(new TicketSelectionController(ticket, this))
+            );
+        }
     }
 }
