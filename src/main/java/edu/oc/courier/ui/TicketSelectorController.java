b50722db8be5b464bfe8253144974e86095b7e1a
Jonathan Troyer <jonathantroyer@outlook.com>
2017-07-05 13:52:44 -0500

    Load tickets as user scrolls to bottom

--- src/main/java/edu/oc/courier/ui/TicketSelectorController.java
+++ src/main/java/edu/oc/courier/ui/TicketSelectorController.java
@@ -6,25 +6,27 @@ import edu.oc.courier.Tuple;
 import edu.oc.courier.data.Ticket;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.ListCell;
-import javafx.scene.control.ListView;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
 import javafx.scene.layout.FlowPane;
 import javafx.util.Callback;
-
-import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 import org.intellij.lang.annotations.Language;
+
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
 
 public class TicketSelectorController implements Initializable {
 
+    @FXML private ScrollPane scroll;
     @FXML private ComboBox<Tuple<String, String>> sortOrder;
     @FXML private ToggleGroup order;
-    @FXML private FlowPane tickets;
+    @FXML private FlowPane ticketsDisplay;
 
     private ContainerController controller;
+    private List<Ticket> tickets;
+    private int loadIndex;
+    private int maxLoadIndex;
+    private boolean allowAdd;
 
     @Override
     public void initialize(final URL location, final ResourceBundle resources) {
@@ -58,6 +60,7 @@ public class TicketSelectorController implements Initializable {
                 new Tuple<>("Actual delivery time", "actualDeliveryTime")
         );
         updateTickets();
+
     }
 
     public void setController(final ContainerController controller) {
@@ -70,17 +73,42 @@ public class TicketSelectorController implements Initializable {
 
     @FXML
     private void updateTickets() {
-        @Language("HQL") String query = "SELECT t FROM Ticket t";
+        loadIndex = 0;
+        maxLoadIndex = 100;
+        this.ticketsDisplay.getChildren().clear();
+        loadTickets();
+    }
+
+    @FXML
+    private void addTickets() {
+        if(allowAdd) {
+            maxLoadIndex += 100;
+            int index = loadIndex;
+            loadTickets();
+            int ticketsPerRow = (int)(scroll.getViewportBounds().getWidth() / 422.0);
+            int startRow = index / ticketsPerRow;
+            scroll.setVvalue(scroll.getViewportBounds().getHeight() / startRow * 162);
+        }
+    }
+
+    private void loadTickets() {
+        allowAdd = false;
+        @Language("HQL") String query = "SELECT t FROM Ticket t WHERE t.id > :loadIndex AND t.id < :maxLoadIndex";
         final Tuple<String, String> field = sortOrder.getValue();
         if (field != null && !"".equals(field.y)) {
             query += " ORDER BY t." + field.y + " " + order.getSelectedToggle().getUserData();
         }
-        this.tickets.getChildren().clear();
         try (DBTransaction transaction = DB.getTransaction()) {
-            transaction.getAll(transaction.query(query, Ticket.class).setMaxResults(100))
-                .forEach(ticket ->
-                    this.tickets.getChildren().add(new TicketSelectionController(ticket, this))
+            tickets = transaction.getAll(
+                    transaction.query(query, Ticket.class)
+                            .setParameter("loadIndex", loadIndex)
+                            .setParameter("maxLoadIndex", maxLoadIndex)
             );
         }
+        for(Ticket t : tickets) {
+            loadIndex++;
+            ticketsDisplay.getChildren().add(new TicketSelectionController(t, this));
+        }
+        allowAdd = true;
     }
 }
